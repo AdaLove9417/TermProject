a5f8e66d121c422b9ecbc09e097bfc1b00c0b053
Adalove9417 <adalove9417@gmail.com>
2018-04-08 14:43:20 -0400

    reduce_max

--- tensorflow-vgg/vgg16.py
+++ tensorflow-vgg/vgg16.py
@@ -89,7 +89,7 @@ class Vgg16:
 
         self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.prob))
         self.max_prob = tf.reduce_max(self.prob, 1)
-        self.diff_max = self.prob - np.repeat(tf.cast(self.max_prob, tf.float32), 8, axis=1)
+        self.diff_max = self.prob - np.repeat(tf.cast(self.max_prob, tf.float32), 8, axis=0)
         self.hot = tf.cast(tf.greater(self.diff_max, 0), tf.float32)
         self.correct_prediction = tf.equal(self.hot, self.y_)
         self.correct_prediction = tf.cast(self.correct_prediction, tf.float32)
