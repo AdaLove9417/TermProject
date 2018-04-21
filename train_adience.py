e68dd916abbd7a192b5a8df86c3b36510c599732
Adalove9417 <adalove9417@gmail.com>
2018-04-21 17:45:28 -0400

    testin

--- train_adience.py
+++ train_adience.py
@@ -22,18 +22,15 @@ pretrain_mode = tf.placeholder(tf.bool)
 levi_hassner = levi_and_hassner.LeviHassner()
 tf = levi_hassner.build(images, learning_rate, 8, tf.constant(True))
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(levi_hassner.cross_entropy)
-
 sess.run(tf.global_variables_initializer())
 
 for i in range(1, num_batches):
     [x_batch, y_batch] = db.sample_train()
-    feed_dict = {levi_hassner.x_: x_batch, levi_hassner.y_: y_batch, train_mode: True, pretrain_mode: False}
-    [_, accuracy, cross_entropy]=sess.run([optimizer, levi_hassner.accuracy, levi_hassner.cross_entropy], feed_dict)
+    [_, accuracy, cross_entropy]=sess.run([levi_hassner.train_op, levi_hassner.accuracy, levi_hassner.cross_entropy], feed_dict={levi_hassner.x_: x_batch, levi_hassner.y_: y_batch, train_mode: True, pretrain_mode: False})
     if i % 500 == 0 or i == 1:
         [x_test_batch, y_test_batch] = db.sample_test()
         feed_dict = {levi_hassner.x_: x_test_batch, levi_hassner.y_: y_test_batch}
-        test_accuracy = sess.run(levi_hassner.accuracy, feed_dict)
+        test_accuracy = sess.run(levi_hassner.accuracy, feed_dict=feed_dict)
         print_str = 'epoch{0} -- train accuracy: {1:.2%} | test accuracy: {2:.2%}'
         print(print_str.format(i, accuracy, test_accuracy))
         levi_hassner.save_npy(sess, npy_path='./imfdb_pretrain-epoch-{0}'.format(i))
