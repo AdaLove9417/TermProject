8a3dc96aab9dd7c1bad8b85dc8a95566c2009bdb
Adalove9417 <adalove9417@gmail.com>
2018-04-21 10:15:33 -0400

    remove learn_rate

--- pretrain_adience_with_imfdb.py
+++ pretrain_adience_with_imfdb.py
@@ -28,7 +28,6 @@ for i in range(1, num_batches):
     sess.run(levi_hassner.train_op, feed_dict)
     accuracy = sess.run(levi_hassner.accuracy, feed_dict)
     cross_entropy = sess.run(levi_hassner.cross_entropy, feed_dict)
-    learn_rate = sess.run(levi_hassner.learn_rate)
     if i % 1000 == 0 or i == 1:
         [x_test_batch, y_test_batch] = db.sample_test()
         feed_dict = {levi_hassner.x_: x_test_batch, levi_hassner.y_: y_test_batch}
@@ -37,4 +36,4 @@ for i in range(1, num_batches):
         print(print_str.format(i, accuracy, test_accuracy))
         levi_hassner.save_npy(sess, npy_path='./imfdb_pretrain-epoch-{0}'.format(i))
     else:
-        print('epoch{0} -- train accuracy: {1:.2%} | loss: {2} | lr: {3}'.format(i, accuracy, cross_entropy, learn_rate))
+        print('epoch{0} -- train accuracy: {1:.2%} | loss: {2}'.format(i, accuracy, cross_entropy))
